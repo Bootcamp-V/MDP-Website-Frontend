a9b290569cbe2b65e8c9c19e1101a7ac0b57b4da
Anthony Javier <90065903+Anthony-Bueno@users.noreply.github.com>
2023-05-05 13:26:41 -0500

    feat:add environment variables

--- src/app/shared/footer/service/footer.service.ts
+++ src/app/shared/footer/service/footer.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { IFooter } from '../model/footer.model';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +12,9 @@ export class FooterService {
   constructor(private http:HttpClient) { }
 
 
+
 getInfoFooter(){
-  return this.http.get<IFooter>("http://localhost:1337/api/footers?populate=*");
+  return this.http.get<IFooter>(environment.baseUrl+"/api/footers?populate=*");
 }
 
 }
