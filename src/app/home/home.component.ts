b64ca35a7c16087182d62a3048a9c0bd74a51966
Anthony Javier <90065903+Anthony-Bueno@users.noreply.github.com>
2023-05-07 21:52:43 -0500

    feat: mofified homePage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { SlideInterface } from '../slider-images/types/slide.interface';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {}
+export class HomeComponent {
+
+  slides: SlideInterface[] = [
+    { url: 'https://www.mdp.com.pe/wp-content/uploads/2018/08/Portada.png' },
+    { url: 'https://www.mdp.com.pe/wp-content/uploads/2017/05/Portada_MDP.jpg' },
+    { url: 'https://www.mdp.com.pe/wp-content/uploads/2017/05/nosotros_MDPAgil.jpg' },
+  ];
+}
