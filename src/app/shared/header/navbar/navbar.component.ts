1002bd82bbf21c398ac8a9c3b0ddd184d40704ad
Alexander Flores <alshmex209@gmail.com>
2023-05-08 17:27:21 -0500

    add new changes to navbar

--- src/app/shared/header/navbar/navbar.component.ts
+++ src/app/shared/header/navbar/navbar.component.ts
@@ -8,51 +8,48 @@ import { IPage } from './models/page.interface';
   styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
-
-  pages !:IPage;
+  pages!: IPage;
   isOpen = false;
-  iconName : string = "hamburger";
-  menuClass : string = "menuppal";
+  iconName: string = 'hamburger';
+  menuClass: string = 'menuppal';
 
-  submenuClass : string = "sub-menupal";
-  openSubmenuServices !: boolean;
-  openSubmenuAbout !: boolean;
-  openSubmenuContact !: boolean;
-  constructor(private navService: NavbarService) {
+  submenuClass: string = 'sub-menupal';
 
-  }
+  openSubmenuServices: boolean = false;
+  openSubmenuAbout: boolean = false;
+  openSubmenuContact: boolean = false;
+  constructor(private navService: NavbarService) {}
   ngOnInit() {
     this.navService.getPages().subscribe((res) => {
-        this.pages = res;      
+      this.pages = res;
     });
   }
 
   toogle() {
     this.isOpen = !this.isOpen;
-    this.iconName = ( this.isOpen ? 'hamburger is-active':'hamburger');
-    this.menuClass = (this.isOpen ? 'menuppal is-active' : 'menuppal');
-    this.openSubmenuServices = false;
-    this.openSubmenuAbout = false;
-    this.openSubmenuContact = false;
-    console.log( "Service:" + this.openSubmenuServices);
+    this.iconName = this.isOpen ? 'hamburger is-active' : 'hamburger';
+    this.menuClass = this.isOpen ? 'menuppal is-active' : 'menuppal';
   }
 
-  toogleSubMenuServices(){
+  toogleSubMenuServices() {
     this.isOpen = true;
     this.openSubmenuServices = !this.openSubmenuServices;
-    this.submenuClass= (this.openSubmenuServices ? 'sub-menupal is-active' : 'sub-menupal')
-    console.log( "Service:" + this.openSubmenuServices);
+    this.submenuClass = this.openSubmenuServices
+      ? 'sub-menupal is-active'
+      : 'sub-menupal';
   }
-  toogleSubMenuAbout(){
+  toogleSubMenuAbout() {
     this.isOpen = true;
     this.openSubmenuAbout = !this.openSubmenuAbout;
-    this.submenuClass= (this.openSubmenuAbout ? 'sub-menupal is-active' : 'sub-menupal')
+    this.submenuClass = this.openSubmenuAbout
+      ? 'sub-menupal is-active'
+      : 'sub-menupal';
   }
-  toogleSubMenuContact(){
+  toogleSubMenuContact() {
     this.isOpen = true;
     this.openSubmenuContact = !this.openSubmenuContact;
-    this.submenuClass= (this.openSubmenuContact ? 'sub-menupal is-active' : 'sub-menupal')
+    this.submenuClass = this.openSubmenuContact
+      ? 'sub-menupal is-active'
+      : 'sub-menupal';
   }
-
 }
- 
