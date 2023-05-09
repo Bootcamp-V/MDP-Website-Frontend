750d7e7cfeea7008f898e03d2356e173fb17ae51
Anthony Javier <90065903+Anthony-Bueno@users.noreply.github.com>
2023-05-09 17:07:36 -0500

    feat:add dinamic component called banner services

--- src/app/services-page/software-development/software-development.component.ts
+++ src/app/services-page/software-development/software-development.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BannerModel } from '../models/banner.model';
 
 @Component({
   selector: 'app-software-development',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class SoftwareDevelopmentComponent {
 
+banner:BannerModel= new BannerModel ('https://www.mdp.com.pe/wp-content/uploads/2017/05/desarrollo_software.jpg',["Desarrollo de Software","Nuestra promesa: Soluciones tecnológicas disruptivas que generan un valor real a su negocio."],
+"Nuestra promesa es entregar soluciones de software con un enfoque diferente, aplicando las mejores prácticas e integrando distintas técnicas que nos permitan superar las expectativas de nuestros clientes y que generen un valor real en las organizaciones y sus clientes en el menor tiempo posible.");
 }
--- src/app/services-page/software-testing/software-testing.component.ts
+++ src/app/services-page/software-testing/software-testing.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BannerModel } from '../models/banner.model';
 
 @Component({
   selector: 'app-software-testing',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class SoftwareTestingComponent {
 
+  banner:BannerModel= new BannerModel ('https://www.mdp.com.pe/wp-content/uploads/2017/05/Prueba-de-Software.jpg',["Pruebas de Software","Nuestra promesa: La calidad es una consecuencia de un gran trabajo y atención por los detalles."],
+  "Hoy, la calidad es uno de los requisitos indispensables para el éxito de un producto, generar una experiencia de uso positiva en los usuarios facilita el incremento de la frecuencia de uso, por lo que cada detalle debe ser analizado cuidadosamente. ");
 }
