6b85135e09e64ad315a21441a4afe4aef7635411
ken  cordero <78985516+togotothemoon@users.noreply.github.com>
2023-05-10 09:15:32 -0500

    fix: routing of aboutUs path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { HomeComponent } from './home/home.component';
 import { ServicesPageComponent } from './services-page/services-page.component';
 import { AboutUsPageComponent } from './about-us-page/about-us-page.component';
 import { OrganizationComponent } from './about-us-page/organization/organization.component';
-
+import { AboutUsComponent } from './about-us-page/about-us/about-us.component';
 import { CustomersPageComponent } from './customers-page/customers-page.component';
 import { BlogPageComponent } from './blog-page/blog-page.component';
 import { ContactPageComponent } from './contact-page/contact-page.component';
@@ -28,14 +28,14 @@ const routes: Routes = [
       { path: 'talento', component: TalentComponent },
     ],
   },
-  { path: 'nosotros', component: AboutUsPageComponent,
+  { path: 'nosotros', component: AboutUsComponent,
     children: [
       {
         path: 'vision-y-proposito',
         component: OrganizationComponent,
       },
       { path: 'historia', component: OrganizationComponent },
-      { path: 'organizacion', component: OrganizationComponent },
+      { path: 'organizacion', component: AboutUsPageComponent },
     ],
   },
   { path: 'clientes', component: CustomersPageComponent },
