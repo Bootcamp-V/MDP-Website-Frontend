c14a388839f3f9f4e31c14454de20f12952f4cc7
ken  cordero <78985516+togotothemoon@users.noreply.github.com>
2023-05-10 09:07:56 -0500

    add routing of aboutUs path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ServicesPageComponent } from './services-page/services-page.component';
 import { AboutUsPageComponent } from './about-us-page/about-us-page.component';
+import { OrganizationComponent } from './about-us-page/organization/organization.component';
+
 import { CustomersPageComponent } from './customers-page/customers-page.component';
 import { BlogPageComponent } from './blog-page/blog-page.component';
 import { ContactPageComponent } from './contact-page/contact-page.component';
@@ -26,7 +28,16 @@ const routes: Routes = [
       { path: 'talento', component: TalentComponent },
     ],
   },
-  { path: 'nosotros', component: AboutUsPageComponent },
+  { path: 'nosotros', component: AboutUsPageComponent,
+    children: [
+      {
+        path: 'vision-y-proposito',
+        component: OrganizationComponent,
+      },
+      { path: 'historia', component: OrganizationComponent },
+      { path: 'organizacion', component: OrganizationComponent },
+    ],
+  },
   { path: 'clientes', component: CustomersPageComponent },
   { path: 'blog', component: BlogPageComponent },
   { path: 'contacto', component: ContactPageComponent },
