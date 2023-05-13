399ecca2fc95b9e82ba45ad64d06ba3c83b1928a
Aldair Chuman <u202017200@upc.edu.pe>
2023-05-13 11:54:41 -0500

    feat: add vision and purpose to strappi

---
+++ src/app/about-us-page/services/values-vision.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { IValuesVision } from '../models/values-vision.interface';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ValuesVisionService {
+
+  constructor(private http: HttpClient) { }
+
+
+  getValuesVision(){
+    return this.http.get<IValuesVision>(environment.devUrl+'/api/values-visions?populate=*');
+  }
+}
