40078356a0a7724473574141b24de575f7bd4feb
Aldair Chuman <u202017200@upc.edu.pe>
2023-05-13 11:55:28 -0500

    "feat: add vision and purpose to strappi"

--- src/app/about-us-page/purpose-vision/components/our-values/our-values/our-values.component.ts
+++ src/app/about-us-page/purpose-vision/components/our-values/our-values/our-values.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IValuesVision } from 'src/app/about-us-page/models/values-vision.interface';
+import { ValuesVisionService } from 'src/app/about-us-page/services/values-vision.service';
 
 @Component({
   selector: 'app-our-values',
   templateUrl: './our-values.component.html',
   styleUrls: ['./our-values.component.scss']
 })
-export class OurValuesComponent {
+export class OurValuesComponent implements OnInit {
+  values$!: Observable<IValuesVision>
 
+  constructor( private servicepage: ValuesVisionService) {
+    
+  }
+
+  ngOnInit():void{
+
+    this.values$= this.servicepage.getValuesVision();
+  }
 }
