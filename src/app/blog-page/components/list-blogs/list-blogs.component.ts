6aa59154de76705aa554a33595feb88006b5f67c
Alexander <alshmex209@gmail.com>
2023-05-29 10:39:06 -0500

    feat: add list-blog's items to html component

--- src/app/blog-page/components/list-blogs/list-blogs.component.ts
+++ src/app/blog-page/components/list-blogs/list-blogs.component.ts
@@ -7,7 +7,7 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./list-blogs.component.scss']
 })
 export class ListBlogsComponent {
-aray:number[]=[1,2,3,4,5,6,7,8,9,10,11,1,1,1,1,1,1,1,1,1,1,1,1,1,11,1,1,1,1,1,1,1,1,1];
+/*aray:number[]=[1,2,3,4,5,6,7,8,9,10,11,1,1,1,1,1,1,1,1,1,1,1,1,1,11,1,1,1,1,1,1,1,1,1];
 
 
 constructor(private router:Router,private routerActive:ActivatedRoute){
@@ -17,6 +17,6 @@ constructor(private router:Router,private routerActive:ActivatedRoute){
 
 getIndex(id:number){
 this.router.navigate([id],{relativeTo:this.routerActive});
-}
+}*/
 
 }
