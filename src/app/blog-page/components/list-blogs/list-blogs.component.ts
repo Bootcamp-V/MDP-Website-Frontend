5b0f611ac4af0400d7f59b2945948df090fbc27d
Anthony Javier <90065903+Anthony-Bueno@users.noreply.github.com>
2023-05-29 17:43:30 -0500

    feat: add conexion with strapi for list blogs

--- src/app/blog-page/components/list-blogs/list-blogs.component.ts
+++ src/app/blog-page/components/list-blogs/list-blogs.component.ts
@@ -1,22 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { BlogPageComponent } from '../../blog-page.component';
+import { BlogService } from '../../services/blog.service';
+import { IBlog } from '../../model/blog.interface';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-list-blogs',
   templateUrl: './list-blogs.component.html',
   styleUrls: ['./list-blogs.component.scss']
 })
-export class ListBlogsComponent {
-/*aray:number[]=[1,2,3,4,5,6,7,8,9,10,11,1,1,1,1,1,1,1,1,1,1,1,1,1,11,1,1,1,1,1,1,1,1,1];
+export class ListBlogsComponent implements OnInit{
+aray:number[]=[1,2,3,4,5,6,7,8,9,10,11,1,1,1,1,1,1,1,1,1,1,1,1,1,11,1,1,1,1,1,1,1,1,1];
+blog$!:Subject<IBlog>
 
+constructor(private router:Router,private routerActive:ActivatedRoute,private servicio:BlogService){
+  this.blog$=new Subject();
+}
+ngOnInit() {
 
-constructor(private router:Router,private routerActive:ActivatedRoute){
+  this.getblogs();
+
 
 }
 
+getblogs(){
+  this.servicio.getBlogs().subscribe(
+    (res)=>{
+      this.servicio.blogs$.next(res);
+      this.blog$.next(res);
+
+  });
+
+}
 
 getIndex(id:number){
 this.router.navigate([id],{relativeTo:this.routerActive});
-}*/
+}
+
+
+
 
 }
--- src/app/blog-page/services/blog.service.ts
+++ src/app/blog-page/services/blog.service.ts
@@ -1,9 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { IBlog } from '../model/blog.interface';
+import { Observable, Subject } from 'rxjs';
+import { environment } from 'src/environments/environment'
 
 @Injectable({
   providedIn: 'root'
 })
 export class BlogService {
 
-  constructor() { }
+  blogs$!:Subject<IBlog>
+
+  constructor(private http: HttpClient) {
+    this.blogs$=new Subject();
+  }
+
+
+
+
+  getBlogs():Observable<IBlog>{
+    return this.http.get<IBlog>(environment.baseUrl+'/api/blogs?populate=*')
+  }
+
+  
 }
