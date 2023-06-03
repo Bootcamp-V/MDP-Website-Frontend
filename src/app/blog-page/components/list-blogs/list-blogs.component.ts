b44830b730df9f587df4488d7c266e35185a3e24
Aldair Chuman <u202017200@upc.edu.pe>
2023-06-02 19:13:22 -0500

    feat: pagination category

--- src/app/blog-page/components/list-blogs/list-blogs.component.ts
+++ src/app/blog-page/components/list-blogs/list-blogs.component.ts
@@ -13,44 +13,61 @@ export class ListBlogsComponent implements OnInit {
   page!: number;
   blog$!: Observable<DataBlog[]>
   arrayblogs: DataBlog[] = [];
+  selectedCategory: string | null = null;
 
   constructor(private router: Router, private routerActive: ActivatedRoute, private servicio: BlogService) {
     this.blog$ = this.servicio.dataBlogsGeneral$
   }
 
-    ngOnInit() {
+  ngOnInit() {
+    this.getblogs();
+  }
 
-      this.getblogs();
+  getblogs() {
+    this.servicio.getBlogs().subscribe(
+      (res) => {
+        this.arrayblogs = res.data;
+        this.generateListByCategory(); 
+        this.page = 1; 
+        this.servicio.dataBlogsGeneral$.next(this.arrayblogs);
+        this.servicio.arrayblogs = this.arrayblogs;
+      }
+    );
+  }
 
+  getIndex(id: number) {
+    this.router.navigate([id], { relativeTo: this.routerActive });
+  }
 
-    }
-
-    getblogs(){
-      this.servicio.getBlogs().subscribe(
-        (res) => {
-
-          for (let i of res.data) {
-            this.arrayblogs.push(i);
-          }
-          this.servicio.dataBlogsGeneral$.next(this.arrayblogs);
-          this.servicio.arrayblogs = this.arrayblogs;
-        });
-    }
-
-    getIndex(id: number){
-      this.router.navigate([id], { relativeTo: this.routerActive });
-    }
-
-    getNameIndex(name: string){
-
-
-      for (let index = 0; index <this.arrayblogs.length; index++) {
-         if(this.arrayblogs[index].attributes.Title===name){
-          this.getIndex(index);
-         }
-
+  getNameIndex(name: string) {
+    for (let index = 0; index < this.arrayblogs.length; index++) {
+      if (this.arrayblogs[index].attributes.Title === name) {
+        this.getIndex(index);
       }
     }
-
-
   }
+
+  getNameDateIndex(name: string, UrlImage: string) {
+    for (let index = 0; index < this.arrayblogs.length; index++) {
+      if (
+        this.arrayblogs[index].attributes.Title === name &&
+        this.arrayblogs[index].attributes.UrlImage === UrlImage
+      ) {
+        this.getIndex(index);
+      }
+    }
+  }
+
+  getCategory(category: string) {
+    this.selectedCategory = category;
+    this.getblogs(); 
+  }
+
+  generateListByCategory() {
+    if (this.selectedCategory) {
+      this.arrayblogs = this.arrayblogs.filter(
+        (item) => item.attributes.Category === this.selectedCategory
+      );
+    }
+  }
+}
