daaa78d9581a5c3091d006bd20eb8168eb122074
Alexander <alshmex209@gmail.com>
2023-06-05 11:21:34 -0500

    feat: add banner in customer's component and add to strapi

--- src/app/customers-page/customers-page.component.ts
+++ src/app/customers-page/customers-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { IDataBP } from '../services-page/models/bannerPages.interface';
+import { map } from 'rxjs';
+import { BannerPagesService } from '../shared/banner-pages/banner-pages.service';
 
 @Component({
   selector: 'app-customers-page',
@@ -6,5 +9,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./customers-page.component.scss']
 })
 export class CustomersPageComponent {
+  banner!: IDataBP;
+
+
+  constructor(private servicio:BannerPagesService) {
+
+  }
+
+
+  ngOnInit() {
+    this.getBannerPage();
+  }
+
+  getBannerPage() {
+    this.servicio.getBannerPage().pipe(
+      map((res) => {
+        for (let i of res.data) {
+
+          if (i.attributes.page == "Customers") {
+            this.banner = i;
+          }
+
+        }
+      })
+
+    ).subscribe((res) => {
+      this.servicio.bannerPages$.next(this.banner);
+    });
+  }
+
 
 }
