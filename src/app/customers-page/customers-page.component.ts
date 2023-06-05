fed6ec25118bf954e3296e1831a517d367af16b8
Flavio Rejas <flaviorejas@gmail.com>
2023-06-05 18:34:27 -0500

    feat: add model and service customers

--- src/app/customers-page/customers-page.component.ts
+++ src/app/customers-page/customers-page.component.ts
@@ -1,24 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IDataBP } from '../services-page/models/bannerPages.interface';
-import { map } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { BannerPagesService } from '../shared/banner-pages/banner-pages.service';
+import { ICustomers } from './models/customers.interface';
+import { CustomersService } from './services/customers.service';
 
 @Component({
   selector: 'app-customers-page',
   templateUrl: './customers-page.component.html',
   styleUrls: ['./customers-page.component.scss']
 })
-export class CustomersPageComponent {
+export class CustomersPageComponent implements OnInit {
   banner!: IDataBP;
 
+  cliBanca$!: Observable<ICustomers>;
+  cliAseguradoras$!: Observable<ICustomers>;
+  cliTelecomunicaciones$!: Observable<ICustomers>;
+  cliRetail$!: Observable<ICustomers>;
+  cliOtros$!: Observable<ICustomers>;
 
-  constructor(private servicio:BannerPagesService) {
+  constructor(private servicio:BannerPagesService, private serviceCustomers: CustomersService) {
 
   }
 
 
   ngOnInit() {
     this.getBannerPage();
+
+    this.cliBanca$ = this.serviceCustomers.getInfoCustomers('Banca');
+    this.cliAseguradoras$ = this.serviceCustomers.getInfoCustomers('Aseguradoras y AFP´s');
+    this.cliTelecomunicaciones$ = this.serviceCustomers.getInfoCustomers('Telecomunicaciones');
+    this.cliRetail$ = this.serviceCustomers.getInfoCustomers('Retail');
+    this.cliOtros$ = this.serviceCustomers.getInfoCustomers('Otros');
+
   }
 
   getBannerPage() {
